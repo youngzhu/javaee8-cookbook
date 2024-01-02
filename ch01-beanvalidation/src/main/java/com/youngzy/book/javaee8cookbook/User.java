3af683a2c5dd2a2d27309b2ee8301f61da696e08
young-win <young.zhu.2012@gmail.com>
2024-01-02 14:22:53 +0800

    Initial commit

---
+++ ch01-beanvalidation/src/main/java/com/youngzy/book/javaee8cookbook/User.java
@@ -0,0 +1,34 @@
+package com.youngzy.book.javaee8cookbook;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author youngzy
+ * @since 2024-01-02
+ */
+public class User {
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @NotEmpty
+    private List<@PositiveOrZero Integer> profileId;
+
+    public User(String name, String email, List<Integer> profileId) {
+        this.name = name;
+        this.email = email;
+        this.profileId = profileId;
+    }
+
+    public User(String name) {
+        this.name = name;
+        this.profileId = Arrays.asList(0);
+    }
+}
---
+++ ch01-beanvalidation/src/test/java/com/youngzy/book/javaee8cookbook/UserTest.java
@@ -0,0 +1,63 @@
+package com.youngzy.book.javaee8cookbook;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    private Validator validator;
+
+    @Before
+    public void setUp() throws Exception {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    public void validUser() {
+        User user = new User(
+                "elder",
+                "elder@eldermoraes.com",
+                asList(1,2));
+
+        Set<ConstraintViolation<User>> cv = validator.validate(user);
+        assertTrue(cv.isEmpty());
+    }
+
+    @Test
+    public void invalidName() {
+        User user = new User("");
+
+        Set<ConstraintViolation<User>> cv = validator.validate(user);
+        assertEquals(1, cv.size());
+    }
+
+    @Test
+    public void invalidEmail() {
+        User user = new User(
+                "elder",
+                "elder-eldermoraes_com",
+                asList(1,2));
+
+        Set<ConstraintViolation<User>> cv = validator.validate(user);
+        assertEquals(1, cv.size());
+    }
+
+    @Test
+    public void invalidId() {
+        User user = new User(
+                "elder",
+                "elder@eldermoraes.com",
+                asList(-1,-2,1,2));
+
+        Set<ConstraintViolation<User>> cv = validator.validate(user);
+        assertEquals(2, cv.size());
+    }
+}
